a00bd84d84ab10dc2604bd0640e496e107720c74
Macce <makkeeh@gmail.com>
2023-11-19 00:37:35 +0200

    vitun nappi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,72 +22,66 @@ const Navbar = ({
   };
   return (
     <>
-      <div className="highScoreButtonContainer">
-        <button
-          className="highScoreButton"
-          style={{
-            transform: highScoreOpen
-              ? "translate(-50%, 405%)"
-              : "translate(-50%, 85%)",
-          }}
-          onClick={handleButtonClick}
-        >
-          <img src={highscorenappi} alt="High Score Button" />
-        </button>{" "}
-        <div className="navbarOuter">
-          <div className="navbar valotausta">
-            <div className="navbarContent">
-              <div className="navbarTop">
-                <div className="navbarLeft">
-                  {" "}
-                  <img src={measureImg}></img>
-                  <div className="sessionLongest">
-                    <p>
-                      SESSION
-                      <br />
-                      LONGEST
-                    </p>
-                    <p className="sesDist">{highScore} M</p>
-                  </div>
-                </div>
-                <div className="navbarCenter">
-                  <img src={logoImg}></img>
-                </div>
-                <div className="navbarRight">
-                  <Settings gameSpeed={gameSpeed} setGameSpeed={setGameSpeed} />
-                  <MusicPlayer mute={mute} setMute={setMute} />
+      <div className="navbarOuter">
+        <div className="navbar valotausta">
+          <div className="navbarContent">
+            <div className="navbarTop">
+              <div className="navbarLeft">
+                {" "}
+                <img src={measureImg}></img>
+                <div className="sessionLongest">
+                  <p>
+                    SESSION
+                    <br />
+                    LONGEST
+                  </p>
+                  <p className="sesDist">{highScore} M</p>
                 </div>
               </div>
-              {highScoreOpen && (
-                <>
-                  <div className="highScoreBox">
-                    <div className="dailyChallenge">
-                      DAILY CHALLENGE
-                      <br />
-                      {dailyChallengeDistance} M <br />
-                      {dailyChallengeData[0].name} with{" "}
-                      {dailyChallengeData[0].distance} M
-                    </div>
-                    <div className="highScoreData">
-                      <HighScoreData highScoreData={highScoreData} />
-                    </div>
-                    <div className="dailyChallengeTop5">
-                      <h2>DAILY CHALLENGE TOP5</h2>
-                      <ul>
-                        {dailyChallengeData.map((entry, index) => (
-                          <li key={index}>
-                            {entry.name}: {entry.distance} M
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  </div>
-                </>
-              )}
+              <div className="navbarCenter">
+                <img src={logoImg}></img>
+              </div>
+              <div className="navbarRight">
+                <Settings gameSpeed={gameSpeed} setGameSpeed={setGameSpeed} />
+                <MusicPlayer mute={mute} setMute={setMute} />
+              </div>
             </div>
+            {highScoreOpen && (
+              <>
+                <div className="highScoreBox">
+                  <div className="dailyChallenge">
+                    DAILY CHALLENGE
+                    <br />
+                    {dailyChallengeDistance} M <br />
+                    {dailyChallengeData[0].name} with{" "}
+                    {dailyChallengeData[0].distance} M
+                  </div>
+                  <div className="highScoreData">
+                    <HighScoreData highScoreData={highScoreData} />
+                  </div>
+                  <div className="dailyChallengeTop5">
+                    <h2>DAILY CHALLENGE TOP5</h2>
+                    <ul>
+                      {dailyChallengeData.map((entry, index) => (
+                        <li key={index}>
+                          {entry.name}: {entry.distance} M
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
+        <div className="highScoreButtonContainer">
+          <button className="highScoreButton" onClick={handleButtonClick}>
+            <img src={highscorenappi} alt="High Score Button" />
+          </button>
+        </div>
       </div>
+      
+      
     </>
   );
 };
