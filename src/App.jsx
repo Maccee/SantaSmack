8b6a431634a9fbd5b524b2417b87854d7285f61a
Macce <makkeeh@gmail.com>
2023-11-21 00:39:52 +0200

    dailychallenge 999

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
 
   // MUSAT
   const [mute, setMute] = useState(false);
-
+  //const [poroHitSound, setPoroHitSound] = useState(new Audio("bells.mp3"));
   // COLLISION
   const [poros, setPoros] = useState([]);
   const ballDiameter = 50;
@@ -92,7 +92,7 @@ const App = () => {
   const [allTimeData, setAllTimeData] = useState({});
   const [weeklyData, setWeeklyData] = useState({});
 
-  const dailyChallengeDistance = 410;
+  const dailyChallengeDistance = 999;
   const [dailyChallengeData, setDailyChallengeData] = useState(null);
 
   // SCROLLING
@@ -295,7 +295,7 @@ const App = () => {
               poroHitAudio = new Audio("hyvahienohomma.mp3");
             }
             if (!mute) {
-              poroHitAudio.play();
+              poroHitSound.play();
             }
 
             hitPorosRef.current.add(index); // Mark this poro as hit
